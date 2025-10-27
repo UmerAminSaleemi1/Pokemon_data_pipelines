fff5ce5029e24a63d25a4339678125fc520f390a
umeraminsaleemi <umeraminsaleemi@gmail.com>
2025-10-27 16:10:59 +0100

    Initial commit

---
+++ app/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    # Use your PostgreSQL connection
+    DATABASE_URL: str = os.getenv(
+        "DATABASE_URL", 
+        "postgresql://postgres:12345@localhost:5432/pokemon_db"
+    )
+    POKEAPI_BASE_URL: str = "https://pokeapi.co/api/v2"
+
+settings = Settings()
